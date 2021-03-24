b679d242de67f720ec8542a0259b9104634a61c4
qn19325 <qn19325@bristol.ac.uk>
2021-03-24 17:19:34 +0000

    Week1 Done

--- Week1/sheet1.py
+++ Week1/sheet1.py
@@ -1,23 +1,35 @@
 import random
 
-def ListToString():
-    return
+def ListToString(list_of_strings):
+    concatenated_string = str()
+    for x in list_of_strings:
+        concatenated_string = concatenated_string + x
+    return concatenated_string
 
 
-def GetEmptyCells():
-    return
+def GetEmptyCells(list_of_strings):
+    list_of_ints = list()
+    for idx, val in enumerate(list_of_strings):
+        if val == ' ':
+            list_of_ints.append(idx)
+    return list_of_ints
 
 
-def GetRandomElement():
-    return
+def GetRandomElement(list):
+    rand_num = random.randrange(0, len(list))  
+    return list[rand_num]
 
 
-def MaxIndices():
-    return
+def MaxIndices(list_of_ints):
+    indexes = list()
+    for idx, val in enumerate(list_of_ints):
+        if val == max(list_of_ints):
+            indexes.append(idx)
+    return indexes
 
 # TESTING
-#print( ListToString(['A','B','C']) == 'ABC' )
-#print( GetEmptyCells([' ','B','C',' ']) == [0,3] ) 
-#print( GetRandomElement([1,2,3,4,5]) )
-#print( MaxIndices([0,8,20,5]) == [2] )
-#print( MaxIndices([7,2,5,7,5]) == [0,3] )
+print( ListToString(['A','B','C']) == 'ABC' )
+print( GetEmptyCells([' ','B','C',' ']) == [0,3] ) 
+print( GetRandomElement([1,2,3,4,5]) )
+print( MaxIndices([0,8,20,5]) == [2] )
+print( MaxIndices([7,2,5,7,5]) == [0,3] )
